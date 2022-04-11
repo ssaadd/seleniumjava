7529adc39433c55f503119cb74729c6882ab67b8
Aleksandr Muravev <muravevkot@gmail.com>
2022-04-11 15:09:34 +0300

    Add Methods for: "go to url" and "wait of element"

--- src/main/java/constants/Constant.java
+++ src/main/java/constants/Constant.java
@@ -2,4 +2,5 @@ package constants;
 
 public class Constant {
     public static final int IMPLICIT_WAIT = 5;
+    public static final int EXPLICIT_WAIT = 10;
 }
--- src/main/java/pages/base/BasePage.java
+++ src/main/java/pages/base/BasePage.java
@@ -1,10 +1,33 @@
 package pages.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static constants.Constant.EXPLICIT_WAIT;
 
 public class BasePage {
     WebDriver driver;
     public BasePage(WebDriver driver) {
         this.driver = driver;
+    }
+
+    /**
+     * The method for navigating to:
+     * @param url
+     */
+    public void goToUrl(String url){
+       driver.get(url);
+    }
+
+    /**
+     * The method for Explicit Wait for visibility of:
+     * @param element
+     * @return
+     */
+    public WebElement waitElementIsVisible(WebElement element){
+        new WebDriverWait(driver, EXPLICIT_WAIT).until(ExpectedConditions.visibilityOf(element));
+        return element;
     }
 }
